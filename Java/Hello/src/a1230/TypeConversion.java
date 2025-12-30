fe8004dd241df2827a0bf1b1b503527cebb47d86
eunsil1 <eipark0114@naver.com>
2025-12-30 09:43:55 +0900

    a1230_1

---
+++ Java/Hello/src/a1230/TypeConversion.java
@@ -0,0 +1,10 @@
+package a1230;
+
+public class TypeConversion {
+  public static void main(String[] args) {
+    System.out.println("===자동 타입 변환===");
+  }
+    // 1. 정수형에서 실수형으로 자동 타입 변환 int -> double
+    int intValue = 100;
+  
+}
